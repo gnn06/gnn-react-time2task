3f980d9f595466e43058680c9a3dd717afecd834
Gilles ORSINI <gilles.orsini@gmail.com>
2023-06-29 11:50:09 +0200

    slotList use Redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import TaskList from './components/tasklist';
 import SlotList from './components/slotlist';
-import { useSelector } from "react-redux";
 
 function App() {
-  const slotRedux = useSelector(state => state.slot.slots);
-  const taskRedux = useSelector(state => state.task.tasks);
   return (
     <div className="flex flex-row">
       <TaskList />
-      <SlotList slots={slotRedux} tasks={taskRedux}/>        
+      <SlotList />        
     </div>
   );
 }
--- src/components/slotlist.jsx
+++ src/components/slotlist.jsx
@@ -1,18 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Slot from './slot';
 
-export default class SlotList extends React.Component {
-    render() {
-        const lStSlot = this.props.slots;
-        const tasks = this.props.tasks;
-
-        return (
-            <div className="m-1">
-                <h1>Slots</h1>
-                {lStSlot.map((slot, index) => {
-                    return <Slot key={slot.id} slot={slot} tasks={tasks}/>
-                })}
-            </div>
+export default function SlotList()  {
+    const slotRedux = useSelector(state => state.slot.slots);
+    const taskRedux = useSelector(state => state.task.tasks);
+    return (
+        <div className="m-1">
+        <h1>Slots</h1>
+        {slotRedux.map((slot, index) => {
+            return <Slot key={slot.id} slot={slot} tasks={taskRedux}/>
+        })}
+        </div>
         )
     }
-}
