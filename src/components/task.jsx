600e9085eff9a6629c8d794374d9fc82300bd4fe
Gilles ORSINI <gilles.orsini@gmail.com>
2023-06-29 15:10:29 +0200

    refactor Task as functioin component

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import './task.css'
 
-export default class Task extends React.Component {
-    render() {
-        return <div className="bg-green-200 border-2 border-gray-500 rounded p-1 my-1">{this.props.task.title}</div>;
-    }
+export default function Task({task}) {
+    return <div className="bg-green-200 border-2 border-gray-500 rounded p-1 my-1">{task.title}</div>;
 }
