e633b46a40b99e5ae4cc72a9e87b76be22e1581b
Gilles ORSINI <gilles.orsini@gmail.com>
2023-07-06 21:23:47 +0200

    add style when task is selected

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { useState } from "react";
 import './task.css'
 
 export default function Task({task}) {
-    return <div className="bg-green-200 border-2 border-gray-500 rounded p-1 my-1">{task.title}</div>;
+    
+    const [selected, setSelected] = useState(false);
+
+    const myClassName = 'rounded p-1 my-1 '
+        + (selected ? 
+           'bg-gray-400  border-gray-700    border-2'
+         : 'bg-green-200 border-gray-500 border-2 ');
+    
+
+    return <div 
+        className={myClassName}>{task.title}</div>;
 }
