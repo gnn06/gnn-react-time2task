4ef023369bbbe1999dd2fc9ead2b41027f3ae541
Gilles ORSINI <gilles.orsini@gmail.com>
2023-07-06 21:37:23 +0200

    add hover on task

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -8,10 +8,13 @@ export default function Task({task}) {
 
     const myClassName = 'rounded p-1 my-1 '
         + (selected ? 
-           'bg-gray-400  border-gray-700    border-2'
-         : 'bg-green-200 border-gray-500 border-2 ');
-    
+           'hover:bg-gray-300  bg-gray-400  border-gray-500 border-2'
+         : 'hover:bg-green-100 bg-green-200 border-gray-500 border-2');
+
+    const onTaskClick = e => setSelected(!selected);
+
 
     return <div 
-        className={myClassName}>{task.title}</div>;
+        className={myClassName}
+        onClick={onTaskClick}>{task.title}</div>;
 }
