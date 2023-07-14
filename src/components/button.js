b2aff6d63db906bf8c24aa83bb0d6b5790a52367
Gilles ORSINI <gilles.orsini@gmail.com>
2023-07-14 15:20:47 +0200

    margin on buttons

--- src/components/button.js
+++ src/components/button.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Button({clickToto, label}) {
     return (
         <button type="button"
-            className="bg-gray-400 hover:bg-gray-500 text-black hover:text-white py-2 px-4 rounded"
+            className="bg-gray-400 hover:bg-gray-500 text-black hover:text-white p-2 m-1 rounded"
             onClick={clickToto}
         >
             {label}
