a495cd39be1b92ecddd58541e38830926c0b740b
Gilles ORSINI <gilles.orsini@gmail.com>
2023-07-14 14:30:07 +0200

    add hover on slot

--- src/components/slot.jsx
+++ src/components/slot.jsx
@@ -13,17 +13,22 @@ export default function Slot({slot}) {
     const association = useSelector(state => state.tasks.association);
     
     const tasksInSlot = findWithSlot(taskRedux, slot.id, association);
-    console.log(tasksInSlot.length);
+    const hasChild = inner == null;
+
+    let slotStyle = "bg-blue-200 border-2 border-gray-500 rounded p-1 my-1 ";
+    if (hasChild) {
+        slotStyle += "hover:bg-blue-100 ";
+    }
 
     return (
-        <div className="bg-blue-200 border-2 border-gray-500 rounded p-1 my-1">
+        <div className={slotStyle}>
         <div className="title">{title}</div>
         {start != null && end != null && <div className="time text-xs">{start} - {end}</div>}
         {inner != null && inner.map((innerSlot, index) => 
             <Slot key={innerSlot.id} slot={innerSlot} />)
         }
         { tasksInSlot.length > 0 && tasksInSlot.map(task => <Task task={task} />)}
-        <div className="h-10"/>
+        <div className="h-10 hover:bg-blue-100"/>
         </div>
         )
     }
