249dd6aca63a3671c42b6c69d2607feca9371243
Gilles ORSINI <gilles.orsini@gmail.com>
2023-08-09 14:56:31 +0200

    replace select filter by text

--- src/components/task-filter.jsx
+++ src/components/task-filter.jsx
@@ -5,18 +5,6 @@ import { setTaskFilter } from "../features/taskSlice";
 
 export default function TaskFilter() {
     
-    const taskFilters= [
-        { label: 'aucun filtre',  value: 'no-filter' },
-        { label: 'semaine',  value: 'week' },
-        { label: 'lundi',    value: 'lundi' },
-        { label: 'mardi',    value: 'mardi' },
-        { label: 'mercredi', value: 'mercredi' },
-        { label: 'jeudi', value: 'jeudi' },
-        { label: 'vendredi', value: 'vendredi' },
-        { label: 'jour',     value: 'slot1' },
-        { label: 'matin',    value: 'slot2' },
-        { label: 'aprem',    value: 'slot3' }
-    ];
     const dispatch = useDispatch();
 
     const onChange = (e) => {
@@ -26,10 +14,9 @@ export default function TaskFilter() {
     
     return <div>
         <label htmlFor="task-filter">Filtre : </label>
-        <select className="p-2 rounded bg-gray-400 hover:bg-gray-300 " id="task-filter" name="task-filter" onChange={onChange}>
-            { taskFilters.map(item => (
-                <option key={item.value} value={item.value} label={item.label}/>
-            ))}
-        </select>
+        <input type="text" 
+            placeholder="expression"
+            className="m-1 p-1"
+            onBlur={onChange}/>
     </div>;
 };
