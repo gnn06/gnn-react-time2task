e40f33a419d98913beb19c87f8d1aafde0e89deb
Gilles ORSINI <gilles.orsini@gmail.com>
2023-11-09 21:36:43 +0100

    WIP

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
 import taskReducer, { setTasks } from '../features/taskSlice';
 import { getAllTasksP } from '../services/base';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
-const store = configureStore({
-  reducer: {
-    tasks: taskReducer
-  }
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, taskReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer
 })
 
-export default store;
+
+export const persistor = persistStore(store)
 
 // retrieve task from DB and set state when done
-getAllTasksP().then(result => store.dispatch(setTasks(result)))
+//getAllTasksP().then(result => store.dispatch(setTasks(result)))
