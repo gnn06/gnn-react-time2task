bfa7ba6cc6444db4627341a3eba1b6177111218a
Gilles ORSINI <gilles.orsini@gmail.com>
2023-11-11 22:18:36 +0100

    create slot.js

--- src/components/domainDataUtil.js
+++ src/components/domainDataUtil.js
@@ -1,3 +1,5 @@
+import { slotIsInOther, completeSlot, slotEqual } from '../services/slot.js';
+
 /**
  * unused
  */
@@ -44,111 +46,6 @@ export function slotMatchExpr(slotId, slotExpr) {
     return result;
 }
 
-/**
- * is a slot is inside an other
- * 'week lundi' is in 'week'
- * module private
- */
-export function slotIsInOther(slotExpr, otherSlotExpr) {
-    if (slotExpr === undefined || otherSlotExpr === undefined) return false;
-    const first = firstSlot(slotExpr)
-    const firstOther = firstSlot(otherSlotExpr)
-    // first level is different, no need to go next level
-    if (first !== firstOther)
-        return false;
-    else {
-        const lower = lowerSlot(slotExpr);
-        const lowerOther = lowerSlot(otherSlotExpr);
-        // other has no more level so previous level egality is enough
-        if (lowerOther === '') 
-            return true;
-        else
-            // need to check at next level
-            return slotIsInOther(lower, lowerOther);   
-    }
-}
-
-/*
- * firstSlot('week lundi') = 'week'
- */
-export function firstSlot(slotExpr) {
-    return slotExpr.split(' ')[0];
-}
-
-/*
- * slotDepth('this_month week lundi') = 3
- * @return int >= 1
- */
-export function slotDepth(slotExpr) {
-    return slotExpr.split(' ').length;
-}
-
-/*
- * lowerSlot('this_month week lundi') = 'week lundi'
- */
-export function lowerSlot(slotExpr) {
-    const i = slotExpr.indexOf(' ');
-    if (i === -1)
-        return '';
-    else
-        return slotExpr.substring(i + 1);
-}
-
-/**
- * completeSlot('lundi') = 'this_month week lundi'
- */
-export function completeSlot(givenSlotExpr) {
-    if (givenSlotExpr === undefined) return undefined;
-    const first = firstSlot(givenSlotExpr);
-    const level = getSlotLevel(first);
-    if (level === 1)
-        return givenSlotExpr;
-    else if (level === 2)
-        return getCurrentSlot(1) + ' ' + givenSlotExpr;
-    else if (level === 3)
-        return getCurrentSlot(1) + ' ' + getCurrentSlot(2) + ' ' + givenSlotExpr;
-    else {
-        return getCurrentSlot(1) + ' ' + getCurrentSlot(2) + ' ' + getCurrentSlot(3) + ' ' + givenSlotExpr;
-    }
-}
-
-/*
- * returns the default slot of the given level
- * getCurrentSlot(1) = this_month
- * this_month, week, lundi, matin
- * level int between 1 this_month and 4 matin
- */
-function getCurrentSlot(level) {
-    if (level === 1)
-        return 'this_month';
-    else if (level === 2)
-        return 'week';
-    else if (level === 3)
-        return 'lundi';
-    else if (level === 4)
-        return 'matin';
-    else
-        return '';
-}
-
-/**
- * give the level of a slot
- * @returns int -1 or between 1 and 4
- */
-function getSlotLevel(slot) {
-    if (slot === 'this_month')
-        return 1;
-    if (slot === 'week' || slot === 'next_week')
-        return 2;
-    else if (slot === 'lundi' || slot === 'mardi' || slot === 'mercredi' || slot === 'jeudi' || slot === 'vendredi')
-        return 3;
-    else if (slot === 'matin' || slot === 'aprem')
-        return 4;
-    else
-        return -1
-}
-
-
 /*
  * unused
  */
@@ -160,29 +57,3 @@ export function setSlotPath(slot, parentPath) {
     }
     return copy;
 }
-
-/**
- * test is two path have the same path
- * @returns boolean
- */
-export function slotEqual(slot, otherSlot) {
-    if (slot !== undefined && otherSlot !== undefined) {
-        const first = firstSlot(slot);
-        const firstOther = firstSlot(otherSlot);
-        if (first !== firstOther)
-            return false;
-        else {
-            const lowerThis = lowerSlot(slot);
-            const lowerOther = lowerSlot(otherSlot);
-            if (lowerThis === '' && lowerOther === '')
-                return true;
-            else
-                return slotEqual(lowerThis, lowerOther);
-        }
-    } else if (slot === undefined && otherSlot === undefined) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
---
+++ src/services/slot.test.js
@@ -0,0 +1,178 @@
+import { slotIsInOther, completeSlot, slotEqual, firstSlot, lowerSlot, slotDepth } from './slot.js';
+
+describe('slotIsInOther', () => {
+    it('slotIsInOther true by first level', () => {
+        const result = slotIsInOther('S32 mercredi matin','S32');
+        expect(result).toBeTruthy();
+    })
+
+    it('slotIsInOther true by second level', () => {
+        const result = slotIsInOther('S32 mercredi matin','S32 mercredi');
+        expect(result).toBeTruthy();
+    })
+
+    it('slotIsInOther true by last level', () => {
+        const result = slotIsInOther('S32 mercredi matin','S32 mercredi matin');
+        expect(result).toBeTruthy();
+    })
+
+    it('slotIsInOther false by last level', () => {
+        const result = slotIsInOther('S32 mercredi aprem','S32 mercredi matin');
+        expect(result).toBeFalsy()
+    })
+
+    it('slotIsInOther false by second level', () => {
+        const result = slotIsInOther('S32 mercredi aprem','S32 lundi aprem');
+        expect(result).toBeFalsy()
+    })
+
+    it('slotIsInOther false by first level', () => {
+        const result = slotIsInOther('S32 mercredi aprem','S33 mercredi aprem');
+        expect(result).toBeFalsy()
+    })
+
+    it('slotIsInOther true other less deeper', () => {
+        const result = slotIsInOther('S32 mercredi aprem','S32 mercredi');
+        expect(result).toBeTruthy()
+    })
+
+    it('slotIsInOther false other less deeper', () => {
+        const result = slotIsInOther('S32 mercredi aprem','S32 lundi');
+        expect(result).toBeFalsy()
+    })
+
+    it('slotIsInOther true other has 1 level', () => {
+        const result = slotIsInOther('S32 mercredi aprem','S32');
+        expect(result).toBeTruthy()
+    })
+
+    it('slotIsInOther false other has 1 level', () => {
+        const result = slotIsInOther('S32 mercredi aprem','S33');
+        expect(result).toBeFalsy()
+    })
+
+    it('slotIsInOther incomplet expr', () => {
+        const result = slotIsInOther('S33','S33');
+        expect(result).toBeTruthy()
+    })
+
+    it('slotIsInOther incomplet expr trop précis', () => {
+        const result = slotIsInOther('S33','S33 mercredi');
+        expect(result).toBeFalsy()
+    })
+
+    it('slotIsInOther incomplet expr false', () => {
+        const result = slotIsInOther('S32 mercredi','S33');
+        expect(result).toBeFalsy()
+    })
+})
+
+describe('firstSlot', () => {
+    it('firstSlot', () => {
+        const result = firstSlot('S32 mercredi matin');
+        expect(result).toEqual('S32')
+    })
+
+    it('firstSlot one level', () => {
+        const result = firstSlot('matin');
+        expect(result).toEqual('matin')
+    })
+
+    it('firstSlot empty', () => {
+        const result = firstSlot('');
+        expect(result).toEqual('')
+    })
+})
+
+describe('lowerSlot', () => {
+    it('lowerSlot', () => {
+        expect(lowerSlot('S32 mercredi matin')).toEqual('mercredi matin')
+    })
+
+    it('lowerSlot no lower', () => {
+        expect(lowerSlot('matin')).toEqual('')
+    })
+
+    it('lowerSlot empty', () => {
+        expect(lowerSlot('')).toEqual('')
+    })
+})
+
+describe('completeSlot', () => {
+    it('completeSlot level1', () => {
+        const result = completeSlot('week');
+        expect(result).toEqual('this_month week');
+    })
+
+    it('completeSlot level2', () => {
+        const result = completeSlot('vendredi');
+        expect(result).toEqual('this_month week vendredi');
+
+    })
+
+    it('completeSlot level3', () => {
+        const result = completeSlot('aprem');
+        expect(result).toEqual('this_month week lundi aprem');
+    })
+
+    it('completeSlot level1 level2', () => {
+        const result = completeSlot('week vendredi');
+        expect(result).toEqual('this_month week vendredi');
+
+    })
+
+    it('completeSlot level1_bis level2', () => {
+        const result = completeSlot('next_week vendredi');
+        expect(result).toEqual('this_month next_week vendredi');
+    })
+
+    it('completeSlot unidefined', () => {
+        const result = completeSlot(undefined);
+        expect(result).toEqual(undefined);
+
+    })
+})
+
+describe('slotDepth', () => {
+    it('one level', () => {
+        const result = slotDepth('week');
+        expect(result).toEqual(1);
+    })
+
+    it('two levels', () => {
+        const result = slotDepth('week lundi');
+        expect(result).toEqual(2);
+    })
+
+    it('three levels', () => {
+        const result = slotDepth('week lundi aprem');
+        expect(result).toEqual(3);
+    })
+})
+
+describe('slotEqual', () => {
+    it('equal one level', () => {
+        const result = slotEqual('week', 'week');
+        expect(result).toEqual(true);
+    })
+    it('not equal one level', () => {
+        const result = slotEqual('week', 'next_week');
+        expect(result).toEqual(false);
+    })
+    it('equal two level', () => {
+        const result = slotEqual('week lundi', 'week lundi');
+        expect(result).toEqual(true);
+    })
+    it('not equal tow level', () => {
+        const result = slotEqual('week lundi', 'week mardi');
+        expect(result).toEqual(false);
+    })
+    it('other empty', () => {
+        const result = slotEqual('week lundi', 'week');
+        expect(result).toEqual(false);
+    })
+    it('this empty', () => {
+        const result = slotEqual('week', 'week lundi');
+        expect(result).toEqual(false);
+    })    
+})
