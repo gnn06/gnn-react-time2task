59bd1b1ba3c3a3b5817b3e1e799fe7379afae59f
Gilles ORSINI <gilles.orsini@gmail.com>
2023-11-19 10:19:35 +0100

    remove unnecessary BDD retrieve tasks at startup

--- src/store/store.js
+++ src/store/store.js
@@ -13,7 +13,3 @@ const store = configureStore({
 })
 
 export default store;
-
-// retrieve task from DB and set state when done
-// TODO check used after migrate to RTK Query
-getAllTasksP().then(result => store.dispatch(setTasks(result)))
