c677f834ea37b607b549634a4036b40f18340a99
Gilles ORSINI <gilles.orsini@gmail.com>
2023-11-25 08:25:55 +0100

    typescript just works

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import store from './store/store'
 import { Provider } from 'react-redux'
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
