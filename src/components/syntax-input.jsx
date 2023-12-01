3c9cacba8b5ceb2186989ed087877f539b130dcb
Gilles ORSINI <gilles.orsini@gmail.com>
2023-12-01 21:17:31 +0100

    show and hide list, append item selected on click

--- src/components/syntax-input.jsx
+++ src/components/syntax-input.jsx
@@ -1,27 +1,41 @@
 import React from "react";
 import { useState } from "react";
-import { AutoComplete } from 'primereact/autocomplete';
 
 export default function SyntaxInput() {
 
   const [items, setItems] = useState(['gilles']);
   const [value, setValue] = useState('exemple');
+  const [show, setShow] = useState(false);
   const search = (event) => {
     setItems(['gilles']);
   }
   const onChange = (e) => {
     console.debug('onChange', e)
-    setValue(e.value)
+    setValue(e.target.value)
   }
   const onSelect = (value) => {
     console.debug('onSelect', value)
   }
 
+  const onItemClick = (e) => {
+    console.debug('click', e.target)
+    setValue(value + e.target.textContent)
+  }
+  const onFocus = () => {
+    setShow(true)
+  }
+  const onBlur = () => {
+    setShow(false)
+  }
+
   return <div>
-      autcocomplete
-      <AutoComplete value={value} 
-        suggestions={items} completeMethod={search} 
-        onChange={onChange}  onSelect={onSelect}/>
+      <input  value={value} onChange={onChange} onFocus={onFocus} onBlur={onBlur} />
+      { show &&  
+      <ul onMouseDown={(e) => e.preventDefault()} >
+        <li onClick={onItemClick}>this_week</li>
+        <li onClick={onItemClick}>mardi</li>
+      </ul>
+      }
     </div>
   ;
 };
