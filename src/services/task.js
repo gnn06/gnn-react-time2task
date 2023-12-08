c9a732f240301bd225a110ed0e497d16a0198b8c
Gilles Orsini <gilles.orsini@gmail.com>
2023-12-08 21:23:36 +0100

    fix order

--- src/services/task.js
+++ src/services/task.js
@@ -1,14 +1,19 @@
-import { slotCompare } from "./slot"
+import { slotCompare, completeSlot, multi2Mono } from "./slot"
 
 export function taskCompare(task1, task2) {
-    const slotComp = slotCompare(task1.slotExpr, task2.slotExpr)
+    // if multi slot, compare on the first one
+    const slotComp = slotCompare(completeSlot(multi2Mono(task1.slotExpr)[0]), completeSlot(multi2Mono(task2.slotExpr)[0]))
     if (slotComp === 0) {
-        if (task1.order < task2.order)
+        // put no order task at begin
+        const order1 = task1.order == undefined ? 0 : task1.order
+        const order2 = task2.order == undefined ? 0 : task2.order
+        if (order1 < order2) {
             return -1
-        else if (task1.order > task2.order)
+        } else if (order1 > order2) {
             return 1
-        else   
+        } else {
             return 0
+        }
     }
     return slotComp
 }
