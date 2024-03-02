912ff6e571f65963b5c38c9d116d00ca04c853d8
Gilles ORSINI <gilles.orsini@gmail.com>
2024-03-02 20:22:15 +0100

    drop view activetasks

--- bddchangelog.sql
+++ bddchangelog.sql
@@ -1,3 +1,7 @@
+//  les dates correspondent au moment de l'exécution en prod
 // filter 'archivé' without managing null value
 alter table tasks alter column "Etat" SET DEFAULT 'A faire';
 update tasks set "Etat" = 'A faire' where "Etat" is null;
+
+// 02/03/2024
+drop view activetasks
