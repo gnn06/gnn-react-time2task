a3a836023c34488e95370c9d548b19695c186288
Gilles ORSINI <gilles.orsini@gmail.com>
2024-09-25 15:57:23 +0200

    adapt build to vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/gnn-react-time2task',
   plugins: [react()],
   css: {
     postcss: {
