01c3cd022ecb74203988f23a97fbfef5ad3c3868
Gilles ORSINI <gilles.orsini@gmail.com>
2024-09-25 15:50:27 +0200

    slow view depth and filters

--- static/js/289.521b52be.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgnn_react_time2task=self.webpackChunkgnn_react_time2task||[]).push([[289],{3289:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=289.521b52be.chunk.js.map
