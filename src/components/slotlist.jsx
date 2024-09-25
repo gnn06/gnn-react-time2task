87684eb31f38345a2ec4093c39ba718d60470090
Gilles ORSINI <gilles.orsini@gmail.com>
2024-09-25 16:01:29 +0200

    put tooltip top

--- src/components/slotlist.jsx
+++ src/components/slotlist.jsx
@@ -21,7 +21,7 @@ export default function SlotList({tasks})  {
     return (
         <div className="m-1 ">
             <Grid container flexDirection="row" justifyContent="end" alignItems="start" gap="0.25em" minHeight={60}>
-                <Tooltip title="Choisir le niveau le plus profond inclus dans la vue.">
+                <Tooltip title="Choisir le niveau le plus profond inclus dans la vue."  placement="top">
                     <Select size="small" defaultValue={conf.levelMaxIncluded === null ? Object.keys(SLOTIDS_BY_LEVEL).length  : conf.levelMaxIncluded} onChange={onChange}>
                         <MenuItem value={1}>Month</MenuItem>
                         <MenuItem value={2}>Week</MenuItem>
