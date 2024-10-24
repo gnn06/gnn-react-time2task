71ba93403c23142cb6de562478bd59a47793dfb3
Gilles Orsini <gilles.orsini@gmail.com>
2024-10-24 18:18:29 +0200

    Test don't have selection state

--- src/components/slotview.jsx
+++ src/components/slotview.jsx
@@ -1,9 +1,23 @@
 import Slot from './slot';
 import { slotViewFilter } from "../data/slot-view";
+import { useState } from 'react';
 
-export default function SlotView({tasks, conf, selection= [], handleSelection})  {
+export default function SlotView({tasks, conf})  {
+    const [initialSelection, setInitialSelection] = useState([])
+    const handleSelection = (path) => {
+        if (initialSelection.indexOf(path) > -1) {
+            setInitialSelection(initialSelection.filter(item => item !== path))
+        } else {
+            setInitialSelection(initialSelection.concat(path))
+        }
+    }
+    
     const slots = slotViewFilter(conf)
-    return slots.map((slot, index) => {
-        return <Slot key={slot.id} slot={slot} tasks={tasks} selection={selection} handleSelection={handleSelection}/>
-    })
+
+    return <div>
+        {slots.map((slot, index) => {
+            return <Slot key={slot.id} slot={slot} tasks={tasks} selection={initialSelection} handleSelection={handleSelection}/>
+        })}
+        { initialSelection }
+    </div>
 }
--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-import { slotViewFilter } from "../data/slot-view";
 import SlotView from "./slotview";
 
 export default function Test() {
-
-    const [initialSelection, setInitialSelection] = useState([ 'this_month this_week jeudi aprem', 'this_month next_week' ])
 
     const conf = {
         collapse: [
@@ -17,18 +13,7 @@ export default function Test() {
         levelMaxIncluded: null
     }
 
-    const handleSelection = (path) => {
-        if (initialSelection.indexOf(path) > -1) {
-            setInitialSelection(initialSelection.filter(item => item !== path))
-        } else {
-            setInitialSelection(initialSelection.concat(path))
-        }
-    }
-    
-    const slot = slotViewFilter(conf)
-
     return <div>
-        <SlotView tasks={[]} conf={conf} selection={initialSelection} handleSelection={handleSelection}/>
-        { initialSelection }
+        <SlotView tasks={[]} conf={conf} />
     </div>
 }
