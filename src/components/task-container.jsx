ff470f2218b01591a4f8c846e7812f8ac7a29c30
Gilles ORSINI <gilles.orsini@gmail.com>
2025-02-09 20:19:24 +0100

    FIX task iinvisible when dragging on other panel

--- src/components/task-container.jsx
+++ src/components/task-container.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 import { filterSlotExpr } from '../data/task.js';
 import { useGetTasksQuery, useUpdateTaskMutation } from "../features/apiSlice.js";
@@ -14,8 +15,14 @@ export default function TaskContainer() {
     const { data:tasksRedux, isLoading, isSuccess } = useGetTasksQuery({userId, activity})
     const currentFilter = useSelector(state => state.tasks.currentFilter);
     const [ updateTask ] = useUpdateTaskMutation()
+    const [isDragging, setDragging] = useState(false)
+
+    const onDndStart = (event) => {
+      setDragging(true)
+    }
 
     const onDnd = (event) => {
+      setDragging(false)
       const source = event.active.id
       const dest   = (event.over && event.over.id) || undefined
       if (dest === undefined) return
@@ -23,24 +30,26 @@ export default function TaskContainer() {
       updateTask({id:source, slotExpr: dest})
     }
 
+    // temporaly change overflow from hidden to visible to make the task visible on the other panel when drraging
+    // panel use overflow:hidden to make panel collapsable.
+    const styleDrag = isDragging ? {overflow:"visible"} : {overflow:"hidden"}
+
     if (!isLoading && isSuccess) {
         const tasksFetched = tasksRedux.slice();
         const tasks = filterSlotExpr(tasksFetched, currentFilter);
         const panel1 = <SlotList tasks={tasks}/>
         const panel2 = <TaskList tasks={tasks}/>
         return (
-          <DndContext onDragEnd={onDnd}>
-            <div className="flex flex-row">
-            {/* <PanelGroup direction="horizontal" className=''> */}
-            {/* <Panel className='' collapsible={true} minSize={20}> */}
-              {panel1}
-            {/* </Panel> */}
-            {/* <PanelResizeHandle className="w-1.5 bg-gray-200 hover:bg-black"/> */}
-            {/* <Panel className='' collapsible={true} minSize={20}> */}
-              {panel2}
-            {/* </Panel>            */}
-            {/* </PanelGroup> */}
-            </div>
+          <DndContext onDragEnd={onDnd} onDragStart={onDndStart}>            
+            <PanelGroup direction="horizontal" className=''>
+              <Panel className='' collapsible={true} minSize={20} >
+                {panel1}
+              </Panel>
+              <PanelResizeHandle className="w-1.5 bg-gray-200 hover:bg-black"/>
+              <Panel className='' collapsible={true} minSize={20} style={styleDrag}>
+                {panel2}
+              </Panel>           
+            </PanelGroup>            
           </DndContext>
         )
     }
