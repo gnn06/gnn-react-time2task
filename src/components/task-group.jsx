8dbef9ed930b7d360b06a90a5b79cc6b89848bba
Gilles ORSINI <gilles.orsini@gmail.com>
2025-02-13 21:21:17 +0100

    colspan group row

--- src/components/task-group.jsx
+++ src/components/task-group.jsx
@@ -5,7 +5,7 @@ import { taskGroup } from '../data/task';
 export default function TaskList({tasks, group, api}) {
     const groupLst = taskGroup(tasks, group)
     return Object.entries(groupLst).map(([key, value]) => <React.Fragment key={key}>
-        { group !== "0" && <tr><td className='pt-2 pb-0.5 text-lg'>{key}</td></tr>}
+        { group !== "0" && <tr><td className='pt-2 pb-0.5 text-lg ' colSpan={10}>{key}</td></tr>}
         { value.map(task => <Task key={task.id} task={task} api={api}/>) }
     </React.Fragment>)    
 }
