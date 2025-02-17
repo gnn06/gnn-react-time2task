2022b1b9e12e13cafd3fb7add73c31fab9819267
Gilles ORSINI <gilles.orsini@gmail.com>
2025-02-17 10:03:21 +0100

    FIX : remove undefined as task group

--- src/components/task-group.jsx
+++ src/components/task-group.jsx
@@ -5,7 +5,7 @@ import { taskGroup } from '../data/task';
 export default function TaskList({tasks, group, api}) {
     const groupLst = taskGroup(tasks, group)
     return Object.entries(groupLst).map(([key, value]) => <React.Fragment key={key}>
-        { group !== "0" && <tr><td className='pt-2 pb-0.5 text-lg ' colSpan={10}>{key}</td></tr>}
+        { (group !== "0" && group !== "Rien" && group !== null) && <tr><td className='pt-2 pb-0.5 text-lg ' colSpan={10}>{key === "undefined" ? "Sans créneau" : key }</td></tr>}
         { value.map(task => <Task key={task.id} task={task} api={api}/>) }
     </React.Fragment>)    
 }
