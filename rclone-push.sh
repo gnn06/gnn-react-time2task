6c24c415a55a831f8dc99d3781a1ba0f9b3002eb
Gilles ORSINI <gilles.orsini@gmail.com>
2026-04-16 21:50:38 +0200

    add hostname into time2task.SYNC_FILE

--- rclone-push.sh
+++ rclone-push.sh
@@ -6,7 +6,7 @@ if ! rclone check time2task.SYNC_FILE onedriveperso:Documents/dev/ >/dev/null 2>
 fi
 
 # avoid newline
-echo -n "$(date --rfc-3339=seconds)" > time2task.SYNC_FILE
+echo -n "$(date --rfc-3339=seconds) $(hostname)" > time2task.SYNC_FILE
 tar cf ../time2task.tar.gz --exclude=node_modules  .
 rclone move --update --progress ../time2task.tar.gz onedriveperso:Documents/dev/
 rclone copy --update --progress ./time2task.SYNC_FILE onedriveperso:Documents/dev/
