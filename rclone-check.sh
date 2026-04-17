072199ca5d7a8428476780bf69351466244300ab
Gilles Orsini <gilles.orsini@gmail.com>
2026-04-17 16:15:12 +0200

    add commits list after sync into rclone-check

--- rclone-check.sh
+++ rclone-check.sh
@@ -7,9 +7,11 @@ if ! rclone check time2task.SYNC_FILE onedriveperso:Documents/dev/ >/dev/null 2>
     echo -n "remote : "
     rclone cat onedriveperso:Documents/dev/time2task.SYNC_FILE
     echo ""
+    git log --after="$(awk '{print $1, $2}' time2task.SYNC_FILE)" --format="%h %ad %s" --date=short
 else
     # modification détectée
     echo -n "onedrive et local synchro : "
     cat time2task.SYNC_FILE
     echo ""
+    git log --after="$(awk '{print $1, $2}' time2task.SYNC_FILE)" --format="%h %ad %s" --date=short
 fi
